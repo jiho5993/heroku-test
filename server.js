bf991d989dc4d694187e566ac5a1bc895a0548b3
jiho5993 <jiho5993@naver.com>
2020-08-19 01:33:42 +0900

    Add eslint

--- server.js
+++ server.js
@@ -1,19 +1,21 @@
-const express = require('express');
-const logger = require('morgan');
+const express = require("express");
+const logger = require("morgan");
+
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
 app.use(logger("dev"));
 
-app.get('/', (req, res) => {
-    res.send("This is main page. Heroku Test");
+app.get("/", (req, res) => {
+  console.log("main");
+  res.send("This is main page. Heroku Test");
 });
 
-app.get('/about', (req, res) => {
-    res.send("made by jiho5993");
+app.get("/about", (req, res) => {
+  res.send("made by jiho5993");
 });
 
 app.listen(PORT, () => {
-    console.log("server on http://localhost:" + PORT);
+  console.log(`server on http://localhost:${PORT}`);
 });
